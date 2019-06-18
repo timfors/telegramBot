fe4a87e629c35e83f9e6d5eee48987ab8d4ffacc
timfors <tkhabibullin2014@litsey2.ru>
2019-06-18 13:42:33 +0300

    origin

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	ucfg := tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	updates, err := bot.GetUpdatesChan(ucfg)
+
+	for {
+		select {
+		case update := <-updates:
+			userName := update.Message.From.UserName
+			chatID := update.Message.Chat.ID
+			text := update.Message.Text
+			log.Printf("[%s] %d %s", userName, chatID, text)
+			reply := "Nu darova"
+			msg := tgbotapi.NewMessage(chatID, reply)
+			bot.Send(msg)
+		}
+	}
+}
